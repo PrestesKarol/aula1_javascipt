e3d567b4e159fd93401f2e6cf5404d1f05404ad3
Karoline Prestes <karolineprestes182@gmail.com>
2023-06-22 17:18:26 -0300

    Entrega aula 1 do curso de javascript

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+// Solicitar dois números
+let num1 = prompt("Olá Padawan, digite um número de sua escolha:");
+let num2 = prompt("Agora digite um segundo número de sua escolha:");
+
+// Converter para números
+let resultado = parseFloat(num1) + parseFloat(num2);
+
+//Mostrar resultado
+alert("A soma dos números escolhidos anteriormente é:" + resultado);
+console.log("A soma dos números escolhidos anteriormente é:" + resultado)
+
+alert("Até mais Padawan!")
